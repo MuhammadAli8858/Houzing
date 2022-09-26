914687e6b3f98b753d71f7fffba043228320edc4
Muhmmad ali <105370368+MuhammadAli8858@users.noreply.github.com>
2022-09-26 19:55:29 +0500

    houzing1

---
+++ src/Components/Navbar/index.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { Outlet, useNavigate } from "react-router-dom";
+import { navbar } from "../../utils/navbar";
+import { Wrapper, Container, Logo, Section, Link } from "./style";
+
+export const Navbar = () => {
+  const navigate = useNavigate();
+  return (
+    <Container>
+      <Wrapper>
+        <Section onClick={() => navigate("/home")} logo>
+          <Logo /> <h3>Houzing</h3>
+        </Section>
+        <Section>
+          {navbar.map(({ title, path }, index) => {
+            return (
+              <Link
+                className={({ isActive }) => isActive && "actives"}
+                key={index}
+                to={path}
+                
+              >
+                {title}
+              </Link>
+            );
+          })}
+        </Section>
+        <Section>
+          <button>Sing in</button>
+        </Section>
+      </Wrapper>
+      <Outlet />
+    </Container>
+  );
+};
+
+export default Navbar;
